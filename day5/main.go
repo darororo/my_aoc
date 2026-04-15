1cd0afba638dc15ba0e3134af1374c221835568d
Yem Daro <daroyem99@gmail.com>
2026-04-15 13:55:19 +0700

    day 5 part 2
    took me like 5 days bruh

--- day5/main.go
+++ day5/main.go
@@ -2,31 +2,25 @@ package main
 
 import (
 	"bufio"
+	"cmp"
 	"fmt"
 	"log"
 	"os"
+	"slices"
 	"strconv"
 	"strings"
 )
 
-func FindFreshIngredients(input string) int {
+var DebugMode bool = false
 
-	parts := strings.Split(input, "\n\n")
-
-	rangeStrs := strings.Split(parts[0], "\n")
-	rangeInts := make([][2]int, len(rangeStrs))
+// Part 1
+func FindFreshIngredients(rangeInts [][2]uint64, ingredients []string) int {
 
 	freshCounts := 0
 
-	for _, str := range rangeStrs {
-		rangeInts = append(rangeInts, parseRange(str))
-	}
-
-	ingredients := strings.SplitSeq(parts[1], "\n")
-
-	for ingredient := range ingredients {
+	for _, ingredient := range ingredients {
 		freshFound := false
-		val, err := strconv.Atoi(string(ingredient))
+		val, err := strconv.ParseUint(ingredient, 10, 64)
 		if err != nil {
 			log.Fatalln(err)
 		}
@@ -48,19 +42,215 @@ func FindFreshIngredients(input string) int {
 	return freshCounts
 }
 
-func parseRange(r string) [2]int {
+// Part 2
+func FindRangesOfGoodIngredients(rangeInts [][2]uint64) ([][2]uint64, uint64) {
+	var total uint64 = 0
+
+	clone := rangeInts
+
+	for {
+		fmt.Println("CLONE")
+		fmt.Println(clone)
+
+		didMerge := false
+
+		for i := 0; i < len(clone); i++ {
+
+			for j := 0; j < len(clone); {
+				if i >= len(clone)-1 {
+					break
+				}
+				fmt.Println("LEN", len(clone))
+
+				fmt.Printf("i %v, j %v R2 %v\n", i, j, clone[j])
+				if i == j {
+					fmt.Println("skipping ", j)
+					j++
+					continue
+				}
+
+				r1 := clone[i]
+				r2 := clone[j]
+
+				merged, ok := MergeRanges(r1, r2)
+				if ok {
+					if DebugMode {
+						fmt.Printf("replacing %v with %v\n", r1, merged)
+					}
+
+					clone[i] = merged
+					if j < len(clone)-1 {
+						if DebugMode {
+							fmt.Printf("removing %v at %v\n", r2, j)
+						}
+
+						clone = append(clone[:j], clone[j+1:]...)
+					} else {
+						if DebugMode {
+							fmt.Printf("removing %v at %v\n", r2, j)
+						}
+
+						clone = clone[:j]
+					}
+					fmt.Println("AFTER REMOVED", clone)
+
+					// j = j - 1
+
+					// fmt.Printf("Delete %v\n", r2)
+
+					didMerge = true
+				} else {
+					j++
+					if DebugMode {
+						fmt.Printf("Merge failed: %v\n", r2)
+					}
+					// failedCounts++
+				}
+			}
+
+		}
+
+		if !didMerge {
+			break
+		}
+
+		if DebugMode {
+			fmt.Printf("Clone: %v\n", clone)
+			// fmt.Printf("Failed merges: %v\n", failedMerges)
+		}
+
+		if len(clone) == 0 {
+			break
+		}
+
+		if DebugMode {
+			fmt.Printf("Clone: %v\n", clone)
+		}
+
+	}
+
+	for _, r := range clone {
+		if DebugMode {
+			fmt.Printf("Optimal: %v\n", r)
+		}
+
+		total += r[1] - r[0] + 1
+	}
+
+	fmt.Println("clone")
+	fmt.Println(clone)
+
+	return clone, total
+}
+
+func parseRange(r string) [2]uint64 {
 	list := strings.Split(r, "-")
-	low, err := strconv.Atoi(list[0])
+	// low, err := strconv.Atoi(list[0])
+	low, err := strconv.ParseUint(list[0], 10, 64)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	high, err := strconv.Atoi(list[1])
+	high, err := strconv.ParseUint(list[1], 10, 64)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	return [2]int{low, high}
+	return [2]uint64{low, high}
+}
+
+func MergeRanges(r1 [2]uint64, r2 [2]uint64) ([2]uint64, bool) {
+
+	// 11-20
+	// 10-12
+	// => can merge [10, 20], true
+
+	// 8-10
+	// 12-20 => cannot merge => [0, 0], false
+
+	// 12-20
+	// 8-10 => cannot merge => [0, 0], false
+
+	findSubset := func(r1 [2]uint64, r2 [2]uint64) ([2]uint64, bool) {
+		isSubset := false
+		l1 := r1[0]
+		h1 := r1[1]
+
+		l2 := r2[0]
+		h2 := r2[1]
+
+		var ld uint64
+		var hd uint64
+
+		if h1 <= h2 {
+			// mergeable
+			// r1: 10-15
+			// r2: 16-20
+			// merged: 10-20
+			if l2-h1 <= 1 {
+				isSubset = true
+
+				ld = l1
+				hd = h2
+			}
+
+			if h1 > l2 {
+				isSubset = true
+				hd = h2
+				if l1 < l2 {
+					// mergeable
+					// r1: 10-15
+					// r2: 12-20
+					// merged: 10-20
+					ld = l1
+				} else {
+					// mergeable
+					// r1: 15-20
+					// r2: 12-20
+					// merged: 12-20
+					ld = l2
+				}
+			}
+		}
+
+		if !isSubset {
+			return [2]uint64{0, 0}, false
+		}
+
+		merged := [2]uint64{ld, hd}
+		return merged, true
+	}
+
+	// try to merge r1 against r2
+	merged, ok := findSubset(r1, r2)
+	if ok {
+		return merged, ok
+	}
+	// return merge result of r2 against r1
+	return findSubset(r2, r1)
+}
+
+func parseInput(input string) ([][2]uint64, []string) {
+	parts := strings.Split(input, "\n\n")
+
+	rangeStrs := strings.Split(parts[0], "\n")
+	var rangeInts [][2]uint64
+
+	for _, str := range rangeStrs {
+		r := parseRange(str)
+		rangeInts = append(rangeInts, r)
+	}
+	ingredients := strings.Split(parts[1], "\n")
+
+	return rangeInts, ingredients
+}
+
+func sortRanges(rangeInts [][2]uint64) [][2]uint64 {
+	slices.SortFunc(rangeInts, func(a, b [2]uint64) int {
+		return cmp.Compare(a[0], b[0])
+	})
+
+	return rangeInts
 }
 
 func main() {
@@ -82,8 +272,19 @@ func main() {
 	input := builder.String()
 	input = input[:len(input)-1]
 
-	counts := FindFreshIngredients(input)
+	rangeInts, ingredients := parseInput(input)
 
-	fmt.Println(counts)
+	part1 := FindFreshIngredients(rangeInts, ingredients)
+	fmt.Printf("Part 1: %v\n", part1)
+
+	// part2 := 0
+	// var totalLast uint64 = 0
+
+	fmt.Printf("length: %v\n", len(rangeInts))
+	fmt.Printf("arr:\n%v\n", rangeInts)
+
+	_, total := FindRangesOfGoodIngredients(rangeInts)
+
+	fmt.Printf("Part 2: \n%v\n", total)
 
 }
--- day5/main_test.go
+++ day5/main_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestFindFreshIngredients(t *testing.T) {
 	input := `3-5
@@ -16,9 +19,87 @@ func TestFindFreshIngredients(t *testing.T) {
 32`
 
 	expected := 3
-	counts := FindFreshIngredients(input)
+	rangeInts, ingredients := parseInput(input)
+	counts := FindFreshIngredients(rangeInts, ingredients)
 	if counts != expected {
 		t.Errorf("Answer: %v, Expected: %v", counts, expected)
 	}
 
+}
+
+func TestFindRangeOfGoodIngredients(t *testing.T) {
+	input := `3-5
+10-14
+16-20
+12-18
+21-25
+3-5
+100-120
+21-25
+3-5
+100-120
+21-25
+3-5
+100-120
+
+1
+5
+8
+11
+17
+32`
+
+	var expected uint64 = 40
+	rangeInts, _ := parseInput(input)
+	ranges, total := FindRangesOfGoodIngredients(rangeInts)
+	if total != expected {
+		t.Errorf("Answer: %v, Expected: %v", total, expected)
+	}
+
+	fmt.Println("Finger")
+	fmt.Println(ranges)
+
+}
+
+func TestMergeRanges(t *testing.T) {
+	tests := []struct {
+		r1   [2]uint64
+		r2   [2]uint64
+		want [2]uint64
+	}{
+		// merge r1 and r2
+		{[2]uint64{10, 20}, [2]uint64{15, 20}, [2]uint64{10, 20}},
+		{[2]uint64{15, 25}, [2]uint64{18, 20}, [2]uint64{15, 25}},
+		{[2]uint64{90, 125}, [2]uint64{100, 120}, [2]uint64{90, 125}},
+		{[2]uint64{10, 18}, [2]uint64{12, 20}, [2]uint64{10, 20}},
+		{[2]uint64{10, 18}, [2]uint64{18, 20}, [2]uint64{10, 20}},
+
+		// swap r1 and r2
+		{[2]uint64{15, 20}, [2]uint64{10, 20}, [2]uint64{10, 20}},
+		{[2]uint64{18, 20}, [2]uint64{15, 25}, [2]uint64{15, 25}},
+		{[2]uint64{100, 120}, [2]uint64{90, 125}, [2]uint64{90, 125}},
+		{[2]uint64{12, 20}, [2]uint64{10, 18}, [2]uint64{10, 20}},
+
+		// r1 = r2
+		{[2]uint64{10, 20}, [2]uint64{10, 20}, [2]uint64{10, 20}},
+
+		// difference of 1 of either lower or upper bounds
+		{[2]uint64{30, 35}, [2]uint64{36, 50}, [2]uint64{30, 50}},
+		{[2]uint64{30, 35}, [2]uint64{10, 29}, [2]uint64{10, 35}},
+		{[2]uint64{10, 20}, [2]uint64{21, 25}, [2]uint64{10, 25}},
+
+		// cannot merge
+		{[2]uint64{40, 60}, [2]uint64{80, 100}, [2]uint64{0, 0}},
+		{[2]uint64{1, 10}, [2]uint64{12, 15}, [2]uint64{0, 0}},
+	}
+
+	for _, tt := range tests {
+		t.Run("testing case", func(t *testing.T) {
+			merged, _ := MergeRanges(tt.r1, tt.r2)
+			if merged != tt.want {
+				t.Errorf("MergeRanges(%v, %v) = %v, want %v;",
+					tt.r1, tt.r2, merged, tt.want)
+			}
+		})
+	}
 }
